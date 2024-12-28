bc38182a039b92d804d0eb5b5ec402978f7539c8
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2024-12-28 22:11:39 +0530

    Developer form and home page with navbar updated and created the routes and nav link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DeveloperForm from './Components/DeveloperForm';
 import ProjectForm from './Components/ProjectForm';
-
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
-        <Route path="/" element={<DeveloperForm />} />
+        <Route path='/' element={<Home/>}/>
+        <Route path="/DeveloperForm" element={<DeveloperForm />} />
         <Route path="/project" element={<ProjectForm />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/Components/DeveloperForm.jsx
+++ frontend/src/Components/DeveloperForm.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { useFormik } from 'formik';
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from 'yup';
 import Select from 'react-select';
 import PhoneInput from "react-phone-input-2";
+import '../assets/CSS/phoneinput.css';
 import "react-phone-input-2/lib/style.css";
-import "../assets/CSS/phoneinput.css"
 
 const skillsOptions = [
     { value: 'JavaScript', label: 'JavaScript' },
@@ -18,20 +17,21 @@ const skillsOptions = [
     { value: 'Java', label: 'Java' },
     { value: 'C++', label: 'C++' },
 ];
-const DeveloperForm = () => {
-    const validationSchema = Yup.object({
-        name: Yup.string().required('Name is required'),
-        email: Yup.string().email('Invalid email address').required('Email is required'),
-        skills: Yup.array().of(Yup.string()).min(1, 'At least one skill is required'),
-        portfolioUrl: Yup.string().required('Portfoliourl required'),
-        githubUrl: Yup.string().required('GithubUrl required'),
-        phone: Yup.string().required('Phone number is required'),
-    });
 
+const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    skills: Yup.array().of(Yup.string()).min(1, 'At least one skill is required'),
+    portfolioUrl: Yup.string().required('Portfoliourl required'),
+    githubUrl: Yup.string().required('GithubUrl required'),
+    phone: Yup.string().required('Phone number is required'),
+});
+
+const DeveloperForm = () => {
     return (
-        <div className="flex justify-center items-center h-screen w-screen bg-black rounded-md text-white font-poppins">
-            <div className=" shadow-xl rounded-lg w-full md:w-1/2 lg:w-1/3">
-                <h1 className="text-center md:text-5xl text-4xl font-bold md:mb-5">Developer Details</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black p-6 text-white rounded-xl">
+            <div className="bg-black shadow-lg rounded-lg p-8 w-full max-w-3xl">
+                <h1 className="text-center font-bold text-3xl">Developer Details</h1>
                 <Formik
                     initialValues={{
                         name: '',
@@ -39,7 +39,7 @@ const DeveloperForm = () => {
                         skills: [],
                         portfolioUrl: '',
                         githubUrl: '',
-                        phone: ''
+                        phone: '',
                     }}
                     validationSchema={validationSchema}
                     onSubmit={(values) => {
@@ -47,22 +47,19 @@ const DeveloperForm = () => {
                     }}
                 >
                     {({ setFieldValue, values }) => (
-                        <Form className="flex flex-col md:mt-5  ">
-                            {/* Full Name Field */}
-                            <label htmlFor="name" className="block font-semibold text-white text-xl mb-2">Full Name</label>
-                            <Field type="text" id="name" name="name" placeholder="Full name" className="md:h-14 h-10 w-full border border-gray-300 bg-transparent p-4 text-white rounded-lg font-poppins block mb-4" />
+                        <Form className='flex flex-col mb-4'>
+
+                            <label className="block font-semibold text-white mb-2">Full Name</label>
+                            <Field type="text" name="name" placeholder="Full name" className="w-full border rounded-lg p-3 outline-none focus:ring focus:ring-blue-300 bg-transparent" />
                             <ErrorMessage name="name" component="div" className="text-red-500 text-sm" />
 
-                            {/* Email Field */}
-                            <label htmlFor="email" className="block font-semibold text-white  text-xl mb-2">Email Address</label>
-                            <Field type="email" id="email" name="email" placeholder="Email Address" className="md:h-14 h-10 w-full border border-gray-300 p-4 bg-transparent  text-white rounded-lg font-poppins block mb-4" />
+                            <label className="block font-semibold text-white mb-2">Email Address</label>
+                            <Field type="email" name="email" placeholder="Email Address" className="w-full border rounded-lg p-3 outline-none focus:ring focus:ring-blue-300 bg-transparent" />
                             <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
 
-                            {/* Skills Field */}
-                            <label htmlFor="skills" className="block font-semibold text-white  text-xl mb-2">Skills</label>
+                            <label className="block font-semibold text-white mb-2">Skills</label>
                             <Select
-                                  className='md:h-14 h-10'
-                                id="skills"
+                                className=""
                                 name="skills"
                                 options={skillsOptions}
                                 isMulti
@@ -72,64 +69,64 @@ const DeveloperForm = () => {
                                 }}
                                 value={values.skills.map(skill => skillsOptions.find(option => option.value === skill))}
                                 styles={{
-                                    control: (styles) => ({ ...styles, backgroundColor: 'transparent', height: '3.5rem', // h-14
-                                        
-                                     }),
+                                    control: (styles) => ({
+                                        ...styles,
+                                        backgroundColor: 'transparent',
+                                        padding: 3,
+                                    }),
                                     option: (styles) => ({
                                         ...styles,
                                         backgroundColor: 'white',
                                         color: 'black',
                                         '&:hover': {
-                                            backgroundColor: 'grey', // slight grey on hover
+                                            backgroundColor: 'grey', // slight grey on hover 
                                         },
                                     }),
-                                    multiValue: (styles) => ({ ...styles, backgroundColor: 'white' }),
-                                    multiValueLabel: (styles) => ({ ...styles, color: 'black' }),
-                                    input: (styles) => ({ ...styles, color: 'white' }), // enter skills on white color
+                                    multiValue: (styles) => ({
+                                        ...styles,
+                                        backgroundColor: 'white'
+                                    }),
+                                    multiValueLabel: (styles) => ({
+                                        ...styles,
+                                        color: 'black'
+                                    }),
+                                    input: (styles) => ({
+                                        ...styles,
+                                        color: 'white' // enter skills on white color 
+                                    }),
                                 }}
 
-                                placeholder="Select Skills"
                             />
-
-
-
                             <ErrorMessage name="skills" component="div" className="text-red-500 text-sm" />
 
-                            {/* PortfolioUrl Field */}
-                            <label htmlFor="portfolioUrl" className="block font-semibold text-white mt-5 md:mt-2  text-xl  md:mb-2 ">PortfolioUrl</label>
-                            <Field type="text" id="portfolioUrl" name="portfolioUrl" placeholder="PortfolioUrl" className="md:h-14 h-10 w-full border bg-transparent border-gray-300 p-4 text-white rounded-lg font-poppins block mb-4" />
+                            <label className="block font-semibold text-white mb-2">PortfolioUrl</label>
+                            <Field type="text" name="portfolioUrl" placeholder="PortfolioUrl" className="w-full border rounded-lg p-3 outline-none focus:ring focus:ring-blue-300 bg-transparent" />
                             <ErrorMessage name="portfolioUrl" component="div" className="text-red-500 text-sm" />
 
-                            {/* GithubUrl Field */}
-                            <label htmlFor="githubUrl" className="block font-semibold text-white  text-xl mb-2">GithubUrl</label>
-                            <Field type="text" id="githubUrl" name="githubUrl" placeholder="GithubUrl" className="md:h-14 h-10 w-full border border-gray-300 bg-transparent p-4 text-white rounded-lg font-poppins block mb-4" />
+                            <label className="block font-semibold text-white mb-2">GithubUrl</label>
+                            <Field type="text" name="githubUrl" placeholder="GithubUrl" className="w-full border rounded-lg p-3 outline-none focus:ring focus:ring-blue-300 bg-transparent" />
                             <ErrorMessage name="githubUrl" component="div" className="text-red-500 text-sm" />
-                            {/* Phone Number Field */}
-
-
-                            <label
-                                htmlFor="phone"
-                                className=" font-semibold text-white text-xl font-poppins md:mt-1 mb-3"
-                            >
-                                Phone Number
-                            </label>
+                            <label className="block font-semibold text-white mb-2">Phone Number</label>
                             <div className="phone-input-container">
-                                <PhoneInput
-                                    country="us"
-                                    value={values.phone}
-                                    onChange={(phone) => setFieldValue("phone", phone)}
-                                    inputProps={{
-                                        name: "phone",
-                                        id: "phone",
-                                        placeholder: "Enter your phone number",
-                                    }}
-                                    inputClass="form-control"
-                                />
-                            </div>
+                                        <PhoneInput
+                                            country="us"
+                                            value={values.phone}
+                                            onChange={(phone) => setFieldValue("phone", phone)}
+                                            inputProps={{
+                                                name: "phone",
+                                                id: "phone",
+                                                placeholder: "Enter your phone number",
+                                            }}
+                                            inputClass="form-control"
+                                            disableCountryCode
+                                        />
+                                    </div>
+
+
                             <ErrorMessage name="phone" component="div" className="text-red-500 text-sm" />
 
-                            {/* Submit Button */}
-                            <button type="submit" className="h-9 w-full md:h-16 bg-[#453FF3] text-white text-2xl font-poppins font-semibold rounded-[20px] mt-6 mb-4">SUBMIT</button>
+
+                            <button type="submit" className="w-full bg-blue-500 text-white py-3 mt-2 rounded-lg font-semibold shadow hover:bg-blue-600 transition duration-200">SUBMIT</button>
                         </Form>
                     )}
                 </Formik>
@@ -139,4 +136,4 @@ const DeveloperForm = () => {
     );
 };
 
-export default DeveloperForm
+export default DeveloperForm;
